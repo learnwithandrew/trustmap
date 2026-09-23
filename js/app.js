f7049ff11492cf88a8c9715965cb76010d6ceb59
andyjohn23 <andrewnick906@gmail.com>
2026-09-23 20:25:03 +0300

    create array of device objects

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const devices = [
+    {
+        id: 1,
+        name: "Amina's Laptop",
+        type: "Laptop",
+        status: "Trusted",
+        ip: "192.168.1.10",
+        access: "Full"
+    },
+
+    {
+        id: 2,
+        name: "Kevin's Phone",
+        type: "Phone",
+        status: "Trusted",
+        ip: "192.168.1.11",
+        access: "Full"
+    },
+
+    {
+        id: 3,
+        name: "Office Printer",
+        type: "Printer",
+        status: "Trusted",
+        ip: "192.168.1.12",
+        access: "Full"
+    },
+
+    {
+        id: 4,
+        name: "Guest Laptop",
+        type: "Laptop",
+        status: "Guest",
+        ip: "192.168.1.20",
+        access: "Restricted"
+    },
+
+    {
+        id: 5,
+        name: "Unknown Device",
+        type: "Unknown",
+        status: "Unknown",
+        ip: "192.168.1.27",
+        access: "Restricted"
+    }
+];
